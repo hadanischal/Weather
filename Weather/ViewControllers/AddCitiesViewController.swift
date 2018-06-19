0828f647ffd4fef1853a732ec27b3e021879e911
Nischal Hada <hadanischal@gmail.com>
2018-06-19 19:34:23 +1000

    Added AddCitiesViewController as Modal view controller

---
+++ Weather/ViewControllers/AddCitiesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddCitiesViewController.swift
+//  Weather
+//
+//  Created by Nischal Hada on 6/19/18.
+//  Copyright © 2018 NischalHada. All rights reserved.
+//
+
+import UIKit
+
+class AddCitiesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
