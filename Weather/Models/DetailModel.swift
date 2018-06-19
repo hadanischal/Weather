ba5a49cbb01a11e2a0b5d44d923fabd96ab3c421
Nischal Hada <hadanischal@gmail.com>
2018-06-19 19:18:34 +1000

    description added in DetailModel

--- Weather/Models/DetailModel.swift
+++ Weather/Models/DetailModel.swift
@@ -10,25 +10,41 @@ import Foundation
 
 struct DetailModel{
     let title: String?
+    let description: String?
     let image: String?
-    init(title: String, image: String?){
+    init(title: String,
+         description: String, image: String?){
         self.title = title
+        self.description = description
         self.image = image
     }
 }
 
 extension DetailModel{
-    static func setupSideBaArray(_ dataSource : WeatherInformation) -> [[DetailModel]] {
+    static func setupDetailModel(_ data : WeatherInformation) -> [[DetailModel]] {
         let header: [DetailModel] = [
-            DetailModel(title: dataSource.weather[0].description, image: "imageMyCompanies"),
+            DetailModel(title:data.weather[0].description, description:"\(data.main.temp)°", image:         WeatherImageProvider().provideValue(data.weather[0].main))
             ]
+        
+        let dateSunrise = NSDate(timeIntervalSince1970: TimeInterval(data.sys.sunrise))
+        let dateSunset = NSDate(timeIntervalSince1970: TimeInterval(data.sys.sunset))
+
         let body: [DetailModel] = [
-            DetailModel(title: "Dashboard", image: "imageDashboard"),
-            DetailModel(title: "Projects", image: "imageProjects"),
-            DetailModel(title: "Work Orders", image: "imageWorkOrders"),
-            DetailModel(title: "My Companies", image: "imageMyCompanies"),
-            DetailModel(title: "Logout", image: "imageLogout")
+            DetailModel(title: "Humidity", description: "\(data.main.humidity)%", image: "Humidity"),
+            DetailModel(title: "Temperature", description: "\(data.main.temp_max)° : \(data.main.temp_min)°", image: "Temperature"),
+            DetailModel(title: "WindSpeed", description: "\(data.wind.speed)m/s", image: "WindSpeed"),
+            DetailModel(title: "Visibility", description: "\(data.visibility)", image: "Visibility"),
+            DetailModel(title: "Sunrise", description: getTimeStringFromDate(date: dateSunrise as Date), image: "Sunrise"),
+            DetailModel(title: "Sunset", description: getTimeStringFromDate(date: dateSunset as Date), image: "Sunset")
         ]
         return [header,body]
+    }
+    
+    static func getTimeStringFromDate(date : Date) -> String{
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .none
+        dateFormatter.timeStyle = .short
+        let dataString = dateFormatter.string(from: date)
+        return dataString
     }
 }
