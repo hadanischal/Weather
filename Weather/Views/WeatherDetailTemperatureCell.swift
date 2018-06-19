cb1454f513ce1f9028d51bfdad0cf2be18467402
Nischal Hada <hadanischal@gmail.com>
2018-06-19 19:21:03 +1000

    implemented WeatherDetailTemperatureCell configureCellWithData

--- Weather/Views/WeatherDetailTemperatureCell.swift
+++ Weather/Views/WeatherDetailTemperatureCell.swift
@@ -9,5 +9,14 @@
 import UIKit
 
 class WeatherDetailTemperatureCell: UICollectionViewCell {
+    @IBOutlet weak var bagroundView: UIView!
+    @IBOutlet var titleLabel: UILabel?
+    @IBOutlet var detailLabel: UILabel?
+    @IBOutlet var weatherImage: UIImageView?
     
+    func configureCellWithData(_ data : DetailModel){
+        titleLabel?.text = data.title
+        detailLabel?.text = data.description
+        weatherImage?.image = UIImage(named: data.image!)
+    }
 }
