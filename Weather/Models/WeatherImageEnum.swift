4e8be4677f3bf05f74b87295779601927691fc28
Nischal Hada <hadanischal@gmail.com>
2018-06-19 19:19:16 +1000

    WeatherImageEnum added for weather type and respective Image

--- Weather/Models/WeatherImageEnum.swift
+++ Weather/Models/WeatherImageEnum.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+enum  WeatherImageEnum: String {
+    case Clouds, Clear, Rain, Squall, Drizzle, Snow, Thunderstorm
+    
+    var create: String {
+        switch self {
+        case .Clouds:
+            return "CloudySky"
+        case .Clear:
+            return "ClearSky"
+        case .Rain:
+            return "RainSky"
+        case .Squall:
+            return "SquallSky"
+        case .Drizzle:
+            return "DrizzleSky"
+        case .Snow:
+            return "SnowSky"
+        case .Thunderstorm:
+            return "ThunderSky"
+        }
+    }
+}
+
+struct WeatherImageProvider {
+    func provideValue(_ weather: String) -> String {
+        if let imageValue = WeatherImageEnum(rawValue: weather){
+            let colorSelected = imageValue.create
+            return colorSelected
+        }else{
+            print("handle invalid weather error")
+            return WeatherImageEnum.Clear.rawValue
+        }
+    }
+}
