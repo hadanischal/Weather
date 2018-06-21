44cb38c5e74c261d061fe6289d6a7e31d19d0e5e
Nischal Hada <hadanischal@gmail.com>
2018-06-21 13:10:23 +1000

    added citylist json file

---
+++ Weather/Extension/FileManager.swift
@@ -0,0 +1,24 @@
+//
+//  FileManager.swift
+//  Weather
+//
+//  Created by Nischal Hada on 6/20/18.
+//  Copyright © 2018 NischalHada. All rights reserved.
+//
+
+import Foundation
+
+
+extension FileManager {
+    static func readJson(_ fileName: String ) -> Data? {
+        let bundle = Bundle(for: File.self)
+        if let path = bundle.path(forResource: fileName, ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                return data
+            } catch {
+            }
+        }
+        return nil
+    }
+}
---
+++ Weather/Extension/citylist.json/File.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  Weather
+//
+//  Created by Nischal Hada on 6/20/18.
+//  Copyright © 2018 NischalHada. All rights reserved.
+//
+
+import Foundation
+
+class File {
+    
+}
